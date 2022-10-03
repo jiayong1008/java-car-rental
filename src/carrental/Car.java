82105787b8640ea23dd1edc87a0524e298579e8f
vinie <vinie@10.101.123.224>
2022-10-03 15:05:12 +0800

    task

---
+++ src/carrental/Car.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrental;
+
+/**
+ *
+ * @author vinie
+ */
+public class Car {
+    private String carPlate;
+    private String carBrand;
+    private double dailyRentalRate;
+    
+    //CONSTUCTORS
+    public Car(){}
+    public Car
+    
+    
+    //GETTERS
+    public String getCarPlate(){return carPlate;}
+    public String getCarBrand(){return carBrand;}
+    public double getDailyRentalRate(){return dailyRentalRate;}
+    
+    //SETTERS
+    public void setCarPlate(String _carPlate) { carPlate = _carPlate; }
+    public void setCarBrand(String _carBrand) { carBrand = _carBrand; }
+    public void setDailyRentalRate(double _dailyRentalRate) { dailyRentalRate = _dailyRentalRate; }
+}
