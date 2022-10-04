d1750243900b70f33cfaeae72ce37185c3c56a0d
vinie <vinie@10.101.123.224>
2022-10-04 12:02:09 +0800

    task

--- src/carrental/Booking.java
+++ src/carrental/Booking.java
@@ -10,7 +10,8 @@ package carrental;
  */
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Booking {
     
@@ -18,15 +19,31 @@ public class Booking {
     private final String bookingId;
     private Customer customer;
     private Car car;
-    private Date bookingDate;
-    private Date startDate;
-    private Date endDate;
+    private LocalDate bookingDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private double bookingFee;
     
     // CONSTRUCTORS
     public Booking(ArrayList<String> bookingInfo)
     {
-        bookingId = bookingInfo.get(2);
+        bookingId = bookingInfo.get(0);
+        String sBookingDate = bookingInfo.get(3);
+        String sstartDate = bookingInfo.get(4);
+        String sendDate = bookingInfo.get(5);
+        
+        //string to double
+        String sBookingFee = bookingInfo.get(6);
+        bookingFee = Double.parseDouble(sBookingFee);
+        
+        //Date Format
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        
+        //Convert String to Date
+        bookingDate = LocalDate.parse(sBookingDate, format);
+        startDate = LocalDate.parse(sstartDate, format);
+        endDate = LocalDate.parse(sendDate, format);
+        
         
     }
     
@@ -34,18 +51,18 @@ public class Booking {
     public String getBookingId() {return bookingId;} 
     public Customer getCustomer() {return customer;} 
     public Car getCar() {return car;}
-    public Date getBookingDate() {return bookingDate;} 
-    public Date getStartDate() {return startDate;} 
-    public Date getEndDate() {return endDate;} 
-    public double getBokkingFee() {return bookingFee;} 
+    public LocalDate getBookingDate() {return bookingDate;} 
+    public LocalDate getStartDate() {return startDate;} 
+    public LocalDate getEndDate() {return endDate;} 
+    public double getBookingFee() {return bookingFee;} 
     
     // SETTERS
     // public void setBookingId(String _bookingId) { bookingId = _bookingId; }
     public void setCustomer(Customer _customer) { customer = _customer; }
     public void setCar(Car _car) { car = _car; }
-    public void setBookingDate(Date _bookingDate) { bookingDate = _bookingDate; }
-    public void setStartDate(Date _startDate) { startDate = _startDate; }
-    public void setEndDate(Date _endDate) { endDate = _endDate; }
+    public void setBookingDate(LocalDate _bookingDate) { bookingDate = _bookingDate; }
+    public void setStartDate(LocalDate _startDate) { startDate = _startDate; }
+    public void setEndDate(LocalDate _endDate) { endDate = _endDate; }
     public void setBookingFee(double _bookingFee) { bookingFee = _bookingFee; }
     
     
--- src/carrental/Car.java
+++ src/carrental/Car.java
@@ -3,28 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package carrental;
+import java.util.ArrayList;
 
-/**
- *
- * @author vinie
- */
+
 public class Car {
     private String carPlate;
     private String carBrand;
+    private String carModel;
     private double dailyRentalRate;
     
     //CONSTUCTORS
-    public Car(){}
-    // public Car
+    public Car(ArrayList<String> carInfo){
+        carPlate = carInfo.get(0);
+        carBrand = carInfo.get(1);
+        carModel = carInfo.get(2);
+        
+        //string to double
+        String sDailyRentalRate = carInfo.get(3);
+        dailyRentalRate = Double.parseDouble(sDailyRentalRate);
+    }
     
     
     //GETTERS
     public String getCarPlate() { return carPlate; }
     public String getCarBrand() { return carBrand; }
+    public String getCarModel() { return carModel; }
     public double getDailyRentalRate() { return dailyRentalRate; }
     
     //SETTERS
     public void setCarPlate(String _carPlate) { carPlate = _carPlate; }
     public void setCarBrand(String _carBrand) { carBrand = _carBrand; }
+    public void setCarModel(String _carModel) { carModel = _carModel; }
     public void setDailyRentalRate(double _dailyRentalRate) { dailyRentalRate = _dailyRentalRate; }
 }
