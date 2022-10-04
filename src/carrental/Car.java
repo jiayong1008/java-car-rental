21d78891edd45d3d9c6a4995b418aff9bb01d202
vinie <vinie@10.101.123.224>
2022-10-04 16:32:43 +0800

    task

--- src/carrental/Car.java
+++ src/carrental/Car.java
@@ -1,10 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package carrental;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 
 public class Car {
     private String carPlate;
